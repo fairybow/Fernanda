23d4190a4b6c2ed1dc1f2b65c0d5a781cac259e8
jackson <63016612+fairybow@users.noreply.github.com>
2023-02-04 16:00:38 -0500

    Update promote.bat

--- fernanda/util/promote.bat
+++ fernanda/util/promote.bat
@@ -2,37 +2,46 @@
 
 :ask_release
 
-set opt_1=
-set /p opt_1=Releasing? (Y/N):
-if not '%opt_1%'=='' set opt_1=%opt_1:~0,1%
-if /i '%opt_1%'=='y' goto promote
-if /i '%opt_1%'=='n' goto promote
-if '%opt_1%'=='' goto promote
-echo "%opt_1%" invalid
+set releasing=
+set /p releasing=Releasing? (Y/N):
+if not '%releasing%'=='' set releasing=%releasing:~0,1%
+if /i '%releasing%'=='y' goto promote
+if /i '%releasing%'=='n' goto promote
+if '%releasing%'=='' goto promote
+echo "%releasing%" invalid
 echo.
 goto ask_release
+
+taskkill devenv.exe
 
 :promote
 
 echo Promoting .
-powershell.exe -ExecutionPolicy ByPass "$shell = New-Object -ComObject 'Shell.Application'; $paths = Get-ChildItem -Path 'C:\Dev\fernanda' -Force -Exclude '.git'; ForEach ($path in $paths) { $shell.NameSpace(0).ParseName($path.FullName).InvokeVerb('delete') }"
+powershell.exe -ExecutionPolicy ByPass ^
+"$shell = New-Object -ComObject 'Shell.Application'; $paths = Get-ChildItem -Path 'C:\Dev\fernanda' -Force -Exclude '.git'; ForEach ($path in $paths) { $shell.NameSpace(0).ParseName($path.FullName).InvokeVerb('delete') }"
 
 echo Promoting . .
-powershell.exe -ExecutionPolicy ByPass "$shell = New-Object -ComObject 'Shell.Application'; $paths = (Get-Item -Path 'C:\Dev\fernanda-dev\x64' -Force), (Get-Item -Path 'C:\Dev\fernanda-dev\fernanda\x64' -Force), (Get-Item -Path 'C:\Dev\fernanda-dev\.vs' -Force); ForEach ($path in $paths) { $shell.NameSpace(0).ParseName($path.FullName).InvokeVerb('delete') }"
+powershell.exe -ExecutionPolicy ByPass ^
+"$shell = New-Object -ComObject 'Shell.Application'; $paths = (Get-Item -Path 'C:\Dev\fernanda-dev\x64' -Force), (Get-Item -Path 'C:\Dev\fernanda-dev\fernanda\x64' -Force), (Get-Item -Path 'C:\Dev\fernanda-dev\.vs' -Force); ForEach ($path in $paths) { $shell.NameSpace(0).ParseName($path.FullName).InvokeVerb('delete') }"
 
 echo Promoting . . .
-powershell.exe -ExecutionPolicy ByPass "Copy-Item -Path 'C:\Dev\fernanda-dev\*' -Destination 'C:\Dev\fernanda' -Exclude '.git' -Recurse"
+powershell.exe -ExecutionPolicy ByPass ^
+"Copy-Item -Path 'C:\Dev\fernanda-dev\*' -Destination 'C:\Dev\fernanda' -Exclude '.git' -Recurse"
 
 echo Promoted!
 
-if /i '%opt_1%'=='n' goto end
-if '%opt_1%'=='' goto end
+if /i '%releasing%'=='n' goto end
+if '%releasing%'=='' goto end
 
-:release
-echo Building release
-start devenv.exe "C:\Dev\fernanda\fernanda.sln" /build Release x64
+:build
+echo Building Visual Studio release . . .
+start /w devenv.exe "C:\Dev\fernanda\fernanda.sln" /build Release x64
 
-:: wait, then start NSIS and build installer
+echo Building NSIS installer . . .
+start /w "C:\Program Files (x86)\NSIS\makensis.exe" "C:\Dev\fernanda\fernanda\util\installer.nsi"
+
+echo f|xcopy /v /y /f "C:\Dev\fernanda\fernanda\util\fernanda-setup-x64-windows.exe" "%OneDrive%\Desktop\fernanda-setup-x64-windows.exe"
+del "C:\Dev\fernanda\fernanda\util\fernanda-setup-x64-windows.exe"
 
 :end
 pause
