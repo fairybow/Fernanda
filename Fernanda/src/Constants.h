9996a2b71c6a1592451426afecf5a2431c6494f8
jackson <63016612+fairybow@users.noreply.github.com>
2025-10-25 11:54:58 -0400

    Update Constants.h

--- Fernanda/src/Constants.h
+++ Fernanda/src/Constants.h
@@ -3,7 +3,6 @@
 // For general, reused, non-translatable strings and any other reused constants
 namespace Fernanda::Constants {
 
-constexpr auto TEMP_DIR_NAME = "temp";
 constexpr auto CONFIG_FILE_NAME = "Settings.ini";
 
 } // namespace Fernanda::Constants
