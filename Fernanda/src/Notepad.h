a36560de1fa94ac6de861c8b2ee173c18917628d
jackson <63016612+fairybow@users.noreply.github.com>
2025-11-15 13:22:48 -0500

    Update Notepad.h

--- Fernanda/src/Notepad.h
+++ Fernanda/src/Notepad.h
@@ -151,11 +151,16 @@ private:
             // by menus
             // - Signal in the ViewService command should take it from
             // there?
+            // 
+            // (Following applies to both WS types)
             //
             // BUT, keep in mind: Before a view is successfully closed:
             // - Notepad: a save prompt may have to appear to reject/accept
             // - Notebook: a persistence autosave may need to happen in the
             // background
+            //
+            // To solve this, could use an interceptor (via Bus or bespoke, like
+            // Window closeAcceptor) or a callback
         });
 
         bus->addCommandHandler(
