1d950b31de99d8c121f7eea8b4d1528ebeef32dd
jackson <63016612+fairybow@users.noreply.github.com>
2025-12-12 20:46:35 -0500

    Adding base NewNotebookPrompt file

---
+++ Fernanda/src/NewNotebookPrompt.h
@@ -0,0 +1,35 @@
+/*
+ * Fernanda  Copyright (C) 2025  fairybow
+ *
+ * Licensed under GPL 3 with additional terms under Section 7. See LICENSE and
+ * ADDITIONAL_TERMS files, or visit: <https://www.gnu.org/licenses/>
+ *
+ * Uses Qt 6 - <https://www.qt.io/>
+ */
+
+#pragma once
+
+#include <QMessageBox>
+#include <QPushButton>
+#include <QString>
+#include <QWidget>
+
+#include "Tr.h"
+
+namespace Fernanda::NewNotebookPrompt {
+
+namespace Internal {
+
+    inline void setCommonProperties_(QMessageBox& box)
+    {
+        //
+    }
+
+} // namespace Internal
+
+inline void exec(QWidget* parent = nullptr)
+{
+    //
+}
+
+} // namespace Fernanda::NewNotebookPrompt
