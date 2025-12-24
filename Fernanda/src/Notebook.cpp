f9871a2f11add452745d38d08967a5b440e00cc5
jackson <63016612+fairybow@users.noreply.github.com>
2025-12-23 19:16:56 -0500

    Removing old note

--- Fernanda/src/Notebook.cpp
+++ Fernanda/src/Notebook.cpp
@@ -28,9 +28,6 @@ void Notebook::createWindowMenuBar_(Window* window)
     // else?)
     // TODO: Somehow section-off common code between Notepad and Notebook
 
-    // TODO: For toggles, will need to store the builders for each window and
-    // call update when current view changes (or other relevant events)?
-
     MenuBuilder(MenuBuilder::MenuBar, window)
         .menu(Tr::nxFileMenu())
 
