36c752c8f16a53f37940ea363e147e7d4bd55e16
jackson <63016612+fairybow@users.noreply.github.com>
2026-01-10 21:28:37 -0500

    More StyleModule setup

--- Fernanda/src/StyleModule.h
+++ Fernanda/src/StyleModule.h
@@ -9,11 +9,16 @@
 
 #pragma once
 
+#include <QList>
 #include <QObject>
+#include <QSet>
 
+#include "AbstractFileView.h"
 #include "AbstractService.h"
 #include "Bus.h"
 #include "Debug.h"
+#include "TextFileView.h"
+#include "Themes.h"
 
 namespace Fernanda {
 
@@ -38,13 +43,46 @@ protected:
 
     virtual void connectBusEvents() override
     {
-        //
+        connect(
+            bus,
+            &Bus::fileViewCreated,
+            this,
+            [&](AbstractFileView* fileView) {
+                // If view is TextFileView, add to tracking QSet
+                // If view is TextFileView, somehow apply current theme to the
+                // editor
+            });
+
+        connect(
+            bus,
+            &Bus::fileViewDestroyed,
+            this,
+            [&](AbstractFileView* fileView) {
+                // Remove from tracking QSet
+            });
     }
 
+    // public theme setting method, maybe (will run through all editors in
+    // tracking QSet and set the theme
+
 private:
+    // QSet<> editors_{};
+
+    qsizetype currentEditorTheme_ =
+        0; /// TODO STYLE: retrieve from settings - may want to do it by name,
+           /// since number of themes can change - when name doesn't match
+           /// anything, use no theme
+    QList<EditorTheme> editorThemes_{};
+
     void setup_()
     {
-        //...
+        // Add "no theme"
+        // editorThemes_ << EditorTheme{};
+        editorThemes_ << EditorTheme{
+            ":/themes/Pocket.fernanda_editor"
+        }; /// <-testing, Pocket as default
+
+        // Later: Read QRC + user data for theme files and parse and store
     }
 };
 
--- Fernanda/src/Themes.h
+++ Fernanda/src/Themes.h
@@ -9,26 +9,92 @@
 
 #pragma once
 
+#include <QByteArray>
+#include <QColor>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QJsonValue>
+#include <QString>
+
+#include "Coco/Path.h"
+
+#include "Debug.h"
+#include "Io.h"
+
 namespace Fernanda {
 
-struct EditorTheme
+class EditorTheme
 {
-    // Example editor theme file contents:
-    /*{
-      "name": "Pocket",
-      "values": {
-        "backgroundColor": "#b6bc9f",
-        "fontColor": "#1b211b",
-        "selectionFontColor": "#b6bc9f",
-        "selectionBgColor": "#1b211b"
-      }
-    }
-    */
+public:
+    EditorTheme() = default;
+    EditorTheme(const Coco::Path& path) { parse_(path); }
 
-    //...
+    ~EditorTheme() { TRACER; }
+
+    bool isValid() const noexcept
+    {
+        return background_.isValid() && font_.isValid() && selection_.isValid()
+               && selectedFont_.isValid();
+    }
+
+private:
+    static constexpr auto NAME_ = "name";
+    QString name_{};
+
+    static constexpr auto VALUES_ = "values";
+
+    static constexpr auto VAR_BG_ = "backgroundColor";
+    QColor background_{};
+
+    static constexpr auto VAR_FONT_ = "fontColor";
+    QColor font_{};
+
+    static constexpr auto VAR_SELECTION_ = "selectionBgColor";
+    QColor selection_{};
+
+    static constexpr auto VAR_SELECTED_FONT_ = "selectionFontColor";
+    QColor selectedFont_{};
+
+    void parse_(const Coco::Path& path)
+    {
+        // Example editor theme file contents:
+        /*{
+          "name": "Pocket",
+          "values": {
+            "backgroundColor": "#b6bc9f",
+            "fontColor": "#1b211b",
+            "selectionFontColor": "#b6bc9f",
+            "selectionBgColor": "#1b211b"
+          }
+        }
+        */
+
+        auto data = Io::read(path);
+
+        QJsonParseError parse_error{};
+        auto document = QJsonDocument::fromJson(data, &parse_error);
+
+        // TODO: debug WARN (maybe INFO, if "no theme" option relies on invalid
+        // theme
+        if (parse_error.error != QJsonParseError::NoError) return;
+        if (!document.isObject()) return;
+
+        auto root = document.object();
+
+        name_ = root[NAME_].toString();
+
+        auto values = root[VALUES_].toObject();
+        if (values.isEmpty()) return;
+
+        background_ = values[VAR_BG_].toString();
+        font_ = values[VAR_FONT_].toString();
+        selection_ = values[VAR_SELECTION_].toString();
+        selectedFont_ = values[VAR_SELECTED_FONT_].toString();
+    }
 };
 
-struct WindowTheme
+class WindowTheme
 {
     //...
 };
