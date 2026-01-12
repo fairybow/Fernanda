b2b00ffc5f69f3bf771759749e29fef787bea5ea
jackson <63016612+fairybow@users.noreply.github.com>
2026-01-11 22:03:17 -0500

    Copying boilerplate from editor theming

--- Fernanda/src/StyleModule.h
+++ Fernanda/src/StyleModule.h
@@ -27,6 +27,7 @@
 #include "Ini.h"
 #include "TextFileView.h"
 #include "Themes.h"
+#include "Window.h"
 
 namespace Fernanda {
 
@@ -47,22 +48,12 @@ public:
 protected:
     virtual void registerBusCommands() override
     {
-        bus->addCommandHandler(Bus::EDITOR_THEMES, [&] {
-            QList<std::pair<QString, Coco::Path>> result{};
-            result.reserve(editorThemes_.size());
-
-            for (auto& theme : editorThemes_)
-                result << std::make_pair(theme.name(), theme.path());
-
-            return result;
+        bus->addCommandHandler(Bus::WINDOW_THEMES, [&] {
+            return themeData_(windowThemes_);
         });
 
-        bus->addCommandHandler(Bus::WINDOW_THEMES, [&] {
-            QList<std::pair<QString, Coco::Path>> result{};
-
-            //...
-
-            return result;
+        bus->addCommandHandler(Bus::EDITOR_THEMES, [&] {
+            return themeData_(editorThemes_);
         });
     }
 
@@ -74,7 +65,31 @@ protected:
             this,
             &StyleModule::onBusSettingChanged_);
 
-        // TODO: Connect new window / lazy load theme
+        connect(bus, &Bus::windowCreated, this, [&](Window* window) {
+            if (!window) return;
+
+            windows_ << window;
+
+            // TODO: Search use of Window::destroyed and replace with this (also
+            // remove the note where I said only use Window::destroyed lol)
+            connect(window, &QObject::destroyed, this, [&, window] {
+                windows_.remove(window);
+            });
+
+            // Lazy-load current theme path if not yet loaded
+            if (!initialWindowThemeLoaded_) {
+                currentWindowThemePath_ = bus->call<QString>(
+                    Bus::GET_SETTING,
+                    { { "key", Ini::Keys::WINDOW_THEME },
+                      { "defaultValue", Ini::Defaults::windowTheme() } });
+
+                initialWindowThemeLoaded_ = true;
+            }
+
+            applyWindowTheme_(
+                window,
+                findWindowTheme_(currentWindowThemePath_));
+        });
 
         connect(
             bus,
@@ -108,25 +123,39 @@ protected:
 
     virtual void postInit() override
     {
+        // Window themes
+
         // TODO: Add user data paths to first arg
-        auto paths = Coco::PathUtil::fromDir(
-            Coco::PathList{ ":/themes/" },
+        auto window_theme_paths = Coco::PathUtil::fromDir(
+            Coco::PathList{ THEMES_QRC_ },
+            WindowTheme::EXT);
+
+        for (auto& path : window_theme_paths)
+            windowThemes_ << WindowTheme{ path };
+
+        sortThemes_(windowThemes_);
+
+        // Editor themes
+
+        // TODO: Add user data paths to first arg
+        auto editor_theme_paths = Coco::PathUtil::fromDir(
+            Coco::PathList{ THEMES_QRC_ },
             EditorTheme::EXT);
 
-        for (auto& path : paths)
+        for (auto& path : editor_theme_paths)
             editorThemes_ << EditorTheme{ path };
 
-        std::sort(
-            editorThemes_.begin(),
-            editorThemes_.end(),
-            [](const EditorTheme& et1, const EditorTheme& et2) {
-                return et1.name().toLower() < et2.name().toLower();
-            });
-
-        // TODO: Window themes
+        sortThemes_(editorThemes_);
     }
 
 private:
+    static constexpr auto THEMES_QRC_ = ":/themes/";
+
+    QSet<Window*> windows_{};
+    QList<WindowTheme> windowThemes_{};
+    bool initialWindowThemeLoaded_ = false;
+    Coco::Path currentWindowThemePath_{};
+
     QSet<TextFileView*> textFileViews_{};
     QList<EditorTheme> editorThemes_{};
     bool initialEditorThemeLoaded_ = false;
@@ -137,7 +166,40 @@ private:
         //...
     }
 
-    EditorTheme findEditorTheme_(const Coco::Path& path)
+    template <typename ThemeT> void sortThemes_(QList<ThemeT>& themes) const
+    {
+        std::sort(
+            themes.begin(),
+            themes.end(),
+            [](const ThemeT& t1, const ThemeT& t2) {
+                return t1.name().toLower() < t2.name().toLower();
+            });
+    }
+
+    template <typename ThemeT>
+    QList<std::pair<QString, Coco::Path>>
+    themeData_(const QList<ThemeT>& themes) const
+    {
+        QList<std::pair<QString, Coco::Path>> result{};
+        result.reserve(themes.size());
+
+        for (auto& theme : themes)
+            result << std::make_pair(theme.name(), theme.path());
+
+        return result;
+    }
+
+    WindowTheme findWindowTheme_(const Coco::Path& path) const
+    {
+        if (path.isEmpty()) return {};
+
+        for (auto& theme : windowThemes_)
+            if (theme.path() == path) return theme;
+
+        return {};
+    }
+
+    EditorTheme findEditorTheme_(const Coco::Path& path) const
     {
         if (path.isEmpty()) return {};
 
@@ -145,6 +207,13 @@ private:
             if (theme.path() == path) return theme;
 
         return {};
+    }
+
+    void applyWindowTheme_(Window* window, const WindowTheme& theme)
+    {
+        if (!window) return;
+
+        // TODO
     }
 
     void applyEditorTheme_(TextFileView* textFileView, const EditorTheme& theme)
@@ -157,32 +226,26 @@ private:
 
     // Find & apply window theme
 
-    /*
-    // Example editor theme (base window theme off this):
-    {
-      "name": "Pocket",
-      "values": {
-        "backgroundColor": "#b6bc9f",
-        "fontColor": "#1b211b",
-        "selectionBgColor": "#1b211b",
-        "selectionFontColor": "#b6bc9f"
-       }
-    }
-    */
-
 private slots:
     // Empty or non-existent path results in no theming
     void onBusSettingChanged_(const QString& key, const QVariant& value)
     {
-        // TODO: Handle window theme
+        if (key == Ini::Keys::WINDOW_THEME) {
 
-        if (key != Ini::Keys::EDITOR_THEME) return;
+            currentWindowThemePath_ = value.value<Coco::Path>();
 
-        currentEditorThemePath_ = value.value<Coco::Path>();
+            auto theme = findWindowTheme_(currentWindowThemePath_);
+            for (auto& window : windows_)
+                applyWindowTheme_(window, theme);
 
-        auto theme = findEditorTheme_(currentEditorThemePath_);
-        for (auto& view : textFileViews_)
-            applyEditorTheme_(view, theme);
+        } else if (key == Ini::Keys::EDITOR_THEME) {
+
+            currentEditorThemePath_ = value.value<Coco::Path>();
+
+            auto theme = findEditorTheme_(currentEditorThemePath_);
+            for (auto& view : textFileViews_)
+                applyEditorTheme_(view, theme);
+        }
     }
 };
 
