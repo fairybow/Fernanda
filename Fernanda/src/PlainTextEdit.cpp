883665f06d3e07b45b6d806db253c13db97dc94c
jackson <63016612+fairybow@users.noreply.github.com>
2026-02-10 22:37:08 -0500

    LineNumberArea draft

--- Fernanda/src/PlainTextEdit.cpp
+++ Fernanda/src/PlainTextEdit.cpp
@@ -9,11 +9,110 @@
 
 #include "PlainTextEdit.h"
 
+#include <QColor>
+#include <QFontMetrics>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QPointF>
+#include <QRect>
+#include <QRectF>
+#include <QString>
+#include <QTextBlock>
+#include <QWidget>
+
 #include "Application.h"
 
 namespace Fernanda {
 
-void PlainTextEdit::onCursorPositionChanged_()
+PlainTextEdit::PlainTextEdit(QWidget* parent)
+    : QPlainTextEdit(parent)
+{
+    setup_();
+}
+
+/// TODO LNA
+void PlainTextEdit::lineNumberAreaPaintEvent(QPaintEvent* event)
+{
+    QPainter painter(lineNumberArea_);
+    /// TODO LNA: Style context color here
+    painter.fillRect(event->rect(), Qt::lightGray);
+
+    auto block = firstVisibleBlock();
+    auto block_number = block.blockNumber();
+    auto top =
+        qRound(blockBoundingGeometry(block).translated(contentOffset()).top());
+    auto bottom = top + qRound(blockBoundingRect(block).height());
+
+    while (block.isValid() && top <= event->rect().bottom()) {
+        if (block.isVisible() && bottom >= event->rect().top()) {
+            auto number = QString::number(block_number + 1);
+            /// TODO LNA: Style context color here
+            /// TODO LNA: Set font, too (same as editor)
+            painter.setPen(Qt::black);
+            painter.drawText(
+                0,
+                top,
+                lineNumberArea_->width(),
+                fontMetrics().height(),
+                Qt::AlignRight,
+                number);
+        }
+
+        block = block.next();
+        top = bottom;
+        bottom = top + qRound(blockBoundingRect(block).height());
+
+        ++block_number;
+    }
+}
+
+/// TODO LNA
+void PlainTextEdit::resizeEvent(QResizeEvent* event)
+{
+    QPlainTextEdit::resizeEvent(event);
+
+    auto cr = contentsRect();
+    lineNumberArea_->setGeometry(
+        { cr.left(), cr.top(), lineNumberAreaWidth(), cr.height() });
+}
+
+void PlainTextEdit::setup_()
+{
+    setViewportMargins(0, 0, 0, 0);
+
+    connect(
+        this,
+        &PlainTextEdit::cursorPositionChanged,
+        this,
+        &PlainTextEdit::resetCursorBlink_);
+
+    /// TODO LNA:
+
+    lineNumberArea_ = new LineNumberArea(this);
+
+    connect(
+        this,
+        &PlainTextEdit::blockCountChanged,
+        this,
+        &PlainTextEdit::updateLineNumberAreaWidth_);
+
+    connect(
+        this,
+        &PlainTextEdit::updateRequest,
+        this,
+        &PlainTextEdit::updateLineNumberArea_);
+
+    connect(
+        this,
+        &PlainTextEdit::cursorPositionChanged,
+        this,
+        &PlainTextEdit::highlightCurrentLine_);
+
+    updateLineNumberAreaWidth_(0);
+    highlightCurrentLine_();
+}
+
+void PlainTextEdit::resetCursorBlink_()
 {
     // Ensuring cursor is visible by just calling `setTextCursor(textCursor())`
     // is not ideal, since though it does work, it doesn't change the
@@ -29,6 +128,18 @@ void PlainTextEdit::onCursorPositionChanged_()
 
     Application::setCursorFlashTime(0);
     Application::setCursorFlashTime(original_flash_time);
+}
+
+/// TODO LNA
+void PlainTextEdit::updateLineNumberArea_(const QRect& rect, int deltaY)
+{
+    if (deltaY)
+        lineNumberArea_->scroll(0, deltaY);
+    else
+        lineNumberArea_
+            ->update(0, rect.y(), lineNumberArea_->width(), rect.height());
+
+    if (rect.contains(viewport()->rect())) updateLineNumberAreaWidth_(0);
 }
 
 } // namespace Fernanda
--- Fernanda/src/PlainTextEdit.h
+++ Fernanda/src/PlainTextEdit.h
@@ -9,84 +9,199 @@
 
 #pragma once
 
+#include <QBrush>
 #include <QChar>
+#include <QColor>
+#include <QFontMetrics>
+#include <QLatin1Char>
+#include <QList>
 #include <QMouseEvent>
+#include <QPaintEvent>
 #include <QPlainTextEdit>
 #include <QPoint>
+#include <QRect>
+#include <QResizeEvent>
+#include <QSize>
 #include <QString>
 #include <QTextCursor>
+#include <QTextEdit>
+#include <QTextFormat>
+#include <QVariant>
 #include <QWidget>
 
 #include "Debug.h"
 
 namespace Fernanda {
 
+class LineNumberArea; /// TODO LNA
+
 // TODO: Check in old "hold" folder for LNA and grabbable highlights code
+// TODO: StyleContext for LNA bg and numbers
+// TODO: Settings INI keys, defaults, and wiring for LNA, line highlight, and
+// whitespace selection
 class PlainTextEdit : public QPlainTextEdit
 {
     Q_OBJECT
 
 public:
-    explicit PlainTextEdit(QWidget* parent = nullptr)
-        : QPlainTextEdit(parent)
+    explicit PlainTextEdit(QWidget* parent = nullptr);
+    virtual ~PlainTextEdit() override { TRACER; }
+
+    bool doubleClickWhitespace() const { return doubleClickWhitespace_; }
+
+    void setDoubleClickWhitespace(bool doubleClickWhitespace)
     {
-        setup_();
+        doubleClickWhitespace_ = doubleClickWhitespace;
     }
 
-    virtual ~PlainTextEdit() override { TRACER; }
+    bool hasLineNumberArea() const { return hasLineNumberArea_; }
+
+    void setHasLineNumberArea(bool hasLineNumberArea)
+    {
+        hasLineNumberArea_ = hasLineNumberArea;
+        /// TODO LNA: Probably update somethin'
+    }
+
+    bool lineHighlight() const { return lineHighlight_; }
+
+    void setLineHighlight(bool lineHighlight)
+    {
+        lineHighlight_ = lineHighlight;
+        /// TODO LNA: Probably update somethin'
+    }
+
+    /// TODO LNA
+    void lineNumberAreaPaintEvent(QPaintEvent* event);
+
+    /// TODO LNA
+    int lineNumberAreaWidth()
+    {
+        auto digits = 1;
+        auto max = qMax(1, blockCount());
+
+        while (max >= 10) {
+            max /= 10;
+            ++digits;
+        }
+
+        auto space =
+            3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+
+        return space;
+    }
 
 protected:
     virtual void mouseDoubleClickEvent(QMouseEvent* event) override
     {
-        // Allow double-clicking white space runs (except single spaces and new
-        // lines)
+        if (doubleClickWhitespace_) {
+            // Allow double-clicking whitespace runs (except single spaces and
+            // new lines)
+            auto cursor = cursorForPosition(event->pos());
+            auto position = static_cast<qsizetype>(cursor.position());
+            auto text = document()->toPlainText();
 
-        /// TODO ES: Make this a toggleable setting
+            if (position < text.size() && text[position].isSpace()
+                && text[position] != '\n') {
+                // Expand left
+                auto start = position;
+                while (start > 0 && text[start - 1].isSpace()
+                       && text[start - 1] != '\n')
+                    --start;
 
-        auto cursor = cursorForPosition(event->pos());
-        auto position = static_cast<qsizetype>(cursor.position());
-        auto text = document()->toPlainText();
+                // Expand right
+                auto end = position;
+                while (end < text.size() && text[end].isSpace()
+                       && text[end] != '\n')
+                    ++end;
 
-        if (position < text.size() && text[position].isSpace()
-            && text[position] != '\n') {
-            // Expand left
-            auto start = position;
-            while (start > 0 && text[start - 1].isSpace()
-                   && text[start - 1] != '\n')
-                --start;
+                if (!(end - start == 1 && text[start] == ' ')) {
+                    cursor.setPosition(start);
+                    cursor.setPosition(end, QTextCursor::KeepAnchor);
+                    setTextCursor(cursor);
 
-            // Expand right
-            auto end = position;
-            while (end < text.size() && text[end].isSpace()
-                   && text[end] != '\n')
-                ++end;
-
-            if (!(end - start == 1 && text[start] == ' ')) {
-                cursor.setPosition(start);
-                cursor.setPosition(end, QTextCursor::KeepAnchor);
-                setTextCursor(cursor);
-
-                return;
+                    return;
+                }
             }
         }
 
         QPlainTextEdit::mouseDoubleClickEvent(event);
     }
 
-private:
-    void setup_()
-    {
-        setViewportMargins(5, 0, 5, 0);
+    /// TODO LNA
+    void resizeEvent(QResizeEvent* event) override;
 
-        connect(
-            this,
-            &PlainTextEdit::cursorPositionChanged,
-            this,
-            &PlainTextEdit::onCursorPositionChanged_);
-    }
+private:
+    /// TODO LNA
+    QWidget* lineNumberArea_ = nullptr;
+
+    bool doubleClickWhitespace_ = true;
+    bool hasLineNumberArea_ = true; /// TODO LNA: impl
+    bool lineHighlight_ = true; /// TODO LNA: impl
+
+    void setup_();
 
 private slots:
-    void onCursorPositionChanged_();
+    void resetCursorBlink_();
+
+    /// TODO LNA
+    void updateLineNumberAreaWidth_(int newBlockCount)
+    {
+        (void)newBlockCount;
+        setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
+    }
+
+    /// TODO LNA
+    void highlightCurrentLine_()
+    {
+        QList<QTextEdit::ExtraSelection> extra_selections{};
+
+        if (!isReadOnly()) {
+            QTextEdit::ExtraSelection selection{};
+            /// TODO LNA: style context value
+            auto line_color = QColor(Qt::yellow).lighter(160);
+
+            selection.format.setBackground(line_color);
+            selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+            selection.cursor = textCursor();
+            selection.cursor.clearSelection();
+
+            extra_selections << selection;
+        }
+
+        setExtraSelections(extra_selections);
+    }
+
+    /// TODO LNA
+    void updateLineNumberArea_(const QRect& rect, int deltaY);
+};
+
+/// TODO LNA
+// See:
+// https://doc.qt.io/archives/qt-5.15/qtwidgets-widgets-codeeditor-example.html
+class LineNumberArea : public QWidget
+{
+public:
+    LineNumberArea(PlainTextEdit* plainTextEdit)
+        : QWidget(plainTextEdit)
+        , plainTextEdit_(plainTextEdit)
+    {
+    }
+
+    virtual ~LineNumberArea() override { TRACER; }
+
+    QSize sizeHint() const override
+    {
+        return { plainTextEdit_->lineNumberAreaWidth(), 0 };
+    }
+
+protected:
+    void paintEvent(QPaintEvent* event) override
+    {
+        plainTextEdit_->lineNumberAreaPaintEvent(event);
+    }
+
+private:
+    PlainTextEdit* plainTextEdit_;
 };
 
 } // namespace Fernanda
