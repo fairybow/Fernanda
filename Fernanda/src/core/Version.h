1e67bded03e4b4804a3dc6ade951238c4b073ea6
jackson <63016612+fairybow@users.noreply.github.com>
2026-03-15 20:41:20 -0400

    Update Version.h

--- Fernanda/src/core/Version.h
+++ Fernanda/src/core/Version.h
@@ -53,7 +53,7 @@ inline constexpr bool isPrerelease = false;
 #define VERSION_MAJOR                           0
 #define VERSION_MINOR                           99
 #define VERSION_PATCH                           0
-#define VERSION_PRERELEASE_STRING               "beta.6"
+#define VERSION_PRERELEASE_STRING               "beta.7"
 
 // Major.Minor.Patch
 #define VERSION_3                               \
