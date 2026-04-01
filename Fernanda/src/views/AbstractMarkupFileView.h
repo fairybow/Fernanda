2ad26e6508b85d66d5238be3452572f2e21ec661
jackson <63016612+fairybow@users.noreply.github.com>
2026-04-01 19:49:06 -0400

    Update AbstractMarkupFileView.h

--- Fernanda/src/views/AbstractMarkupFileView.h
+++ Fernanda/src/views/AbstractMarkupFileView.h
@@ -12,10 +12,14 @@
 
 #pragma once
 
+#include <QHBoxLayout>
 #include <QScrollBar>
 #include <QSplitter>
+#include <QStackedWidget>
 #include <QString>
 #include <QTextDocument>
+#include <QToolButton>
+#include <QVBoxLayout>
 #include <QWebEngineView>
 #include <QWidget>
 
@@ -26,13 +30,6 @@
 
 namespace Fernanda {
 
-// TODO: Start edit only and load preview in the background? It's only slow in
-// debug, though
-// TODO: Toggle/cycle button
-// TODO: ^ Would prefer more distinctive toggle modes (not moving a splitter);
-// possibly a stacked widget that displays the right modes between edit, split,
-// and preview. Though, this would mean moving the two widgets into a splitter
-// for split or having duplicates?
 class AbstractMarkupFileView : public TextFileView
 {
     Q_OBJECT
@@ -40,8 +37,8 @@ class AbstractMarkupFileView : public TextFileView
 public:
     enum Mode
     {
+        Edit = 0,
         Split,
-        Edit,
         Preview
     };
 
@@ -65,24 +62,37 @@ public:
 
         switch (mode) {
 
-        default:
-        case Split:
+        case Edit: {
             editor->show();
-            preview_->show();
+            previewStack_->hide();
             splitter_->setFocusProxy(editor);
-            break;
 
-        case Edit:
+            break;
+        }
+
+        case Split: {
             editor->show();
-            preview_->hide();
-            splitter_->setFocusProxy(editor);
-            break;
+            previewStack_->show();
 
-        case Preview:
+            if (needsInitialSplit_) {
+                previewStack_->setMinimumWidth(splitter_->width() / 2);
+                previewStack_->setMinimumWidth(MIN_WIDGET_WIDTH_);
+
+                needsInitialSplit_ = false;
+            }
+
+            splitter_->setFocusProxy(editor);
+
+            break;
+        }
+
+        case Preview: {
             editor->hide();
-            preview_->show();
-            splitter_->setFocusProxy(preview_);
+            previewStack_->show();
+            splitter_->setFocusProxy(previewStack_);
+
             break;
+        }
         }
 
         if (mode != Edit) reparse_();
@@ -111,14 +121,44 @@ protected:
     virtual QWidget* setupWidget() override
     {
         auto editor_widget = TextFileView::setupWidget();
+        editor_widget->setMinimumWidth(MIN_WIDGET_WIDTH_);
 
-        preview_ = new QWebEngineView(this);
+        modeBar_->setFixedHeight(24);
+
+        toggleButton_->setText(
+            "Toggle Mode"); /// TODO MU: Reactive icon or switch
+        toggleButton_->setFixedHeight(20);
+
+        previewLoadingMask_->setStyleSheet(
+            QStringLiteral("background-color: black;"));
         preview_->setPage(new MarkupPreviewPage(preview_));
 
-        splitter_ = new QSplitter(Qt::Horizontal, this);
+        previewStack_->addWidget(previewLoadingMask_); // 0
+        previewStack_->addWidget(preview_); // 1
+        previewStack_->setCurrentWidget(previewLoadingMask_);
+
         splitter_->addWidget(editor_widget);
-        splitter_->addWidget(preview_);
-        splitter_->setFocusProxy(editor_widget);
+        splitter_->addWidget(previewStack_);
+
+        // Needed to allow the min size setting in Split case above to not push
+        // splitter handle too far
+        splitter_->setStretchFactor(0, 1);
+        splitter_->setStretchFactor(1, 0);
+        splitter_->setChildrenCollapsible(false);
+
+        auto container_layout = new QVBoxLayout(container_);
+        container_layout->setContentsMargins(0, 0, 0, 0);
+        container_layout->setSpacing(0);
+
+        auto mode_bar_layout = new QHBoxLayout(modeBar_);
+        mode_bar_layout->setContentsMargins(0, 2, 2, 2);
+        mode_bar_layout->setSpacing(0);
+
+        mode_bar_layout->addStretch();
+        mode_bar_layout->addWidget(toggleButton_, 0);
+
+        container_layout->addWidget(modeBar_, 0);
+        container_layout->addWidget(splitter_, 1);
 
         connect(
             editor()->document(),
@@ -128,9 +168,24 @@ protected:
                 if (preview_ && preview_->isVisible()) reparseTimer_->start();
             });
 
-        reparse_();
+        connect(toggleButton_, &QToolButton::clicked, this, [this] {
+            cycleMode();
+        });
 
-        return splitter_;
+        connect(preview_, &QWebEngineView::loadStarted, this, [this] {
+            previewStack_->setCurrentWidget(previewLoadingMask_);
+        });
+
+        connect(preview_, &QWebEngineView::loadFinished, this, [this](bool) {
+            //previewStack_->setCurrentWidget(preview_);
+            Time::delay(50, this, [this] {
+                previewStack_->setCurrentWidget(preview_);
+            });
+        });
+
+        setMode(Edit);
+
+        return container_;
     }
 
     // Subclasses implement this to convert plain text to HTML for the preview
@@ -139,26 +194,25 @@ protected:
     QWebEngineView* preview() const noexcept { return preview_; }
 
 private:
-    QSplitter* splitter_ = nullptr;
-    QWebEngineView* preview_ = nullptr;
-    Mode mode_ = Split;
+    Mode mode_ = Edit;
+    QWidget* container_ = new QWidget(this);
+    QWidget* modeBar_ = new QWidget(this);
+    QToolButton* toggleButton_ = new QToolButton(this);
+    QSplitter* splitter_ = new QSplitter(Qt::Horizontal, this);
+    QStackedWidget* previewStack_ = new QStackedWidget(this);
+    QWidget* previewLoadingMask_ =
+        new QWidget(this); /// Make white and maybe add spinner
+    QWebEngineView* preview_ = new QWebEngineView(this);
+
     Time::Debouncer* reparseTimer_ =
         Time::newDebouncer(this, &AbstractMarkupFileView::reparse_, 250);
 
+    constexpr static auto MIN_WIDGET_WIDTH_ = 50;
+    bool needsInitialSplit_ = true;
+    bool initialLoadDone_ = false;
+
     static QString appFontFaceKit_();
 
-    // void reparse_()
-    //{
-    //     auto editor = this->editor();
-    //     if (!preview_ || !editor) return;
-
-    //    auto scroll = preview_->verticalScrollBar()->value();
-    //    auto html = renderToHtml(editor->document()->toPlainText());
-    //    preview_->setHtml(html);
-    //    preview_->verticalScrollBar()->setValue(scroll);
-    //}
-
-    // TODO: This is buggy and jumpy af
     void reparse_()
     {
         auto editor = this->editor();
@@ -166,35 +220,38 @@ private:
 
         auto html = renderToHtml(editor->document()->toPlainText());
 
-        // Inject bundled font-face rules for the web engine
         html.replace(
             QStringLiteral("</head>"),
             QStringLiteral("<style>%1</style></head>").arg(appFontFaceKit_()));
 
-        if (preview_->url().isEmpty()) {
-            // First load (no scroll to preserve)
-            preview_->setHtml(
-                html,
-                QUrl("qrc:/")); /// TODO MU: I am vaguely concerned about this
+        if (!initialLoadDone_) {
+            initialLoadDone_ = true;
+            preview_->setHtml(html, QUrl("qrc:/"));
             return;
         }
 
-        // Subsequent loads (preserve scroll)
-        preview_->page()->runJavaScript(
-            "window.scrollY",
-            [this, html](const QVariant& scrollPos) {
-                auto y = scrollPos.toInt();
-                preview_->setHtml(html, QUrl("qrc:/")); /// TODO MU: See above
-                connect(
-                    preview_,
-                    &QWebEngineView::loadFinished,
-                    this,
-                    [this, y](bool) {
-                        preview_->page()->runJavaScript(
-                            QString("window.scrollTo(0, %1)").arg(y));
-                    },
-                    Qt::SingleShotConnection);
-            });
+        // Extract just the body content and swap it via DOM manipulation
+        auto body_start = html.indexOf(QStringLiteral("<body>"));
+        auto body_end = html.indexOf(QStringLiteral("</body>"));
+
+        if (body_start < 0 || body_end < 0) {
+            preview_->setHtml(html, QUrl("qrc:/"));
+            return;
+        }
+
+        auto body_content = html.mid(body_start + 6, body_end - body_start - 6);
+
+        // Escape for JS string
+        body_content.replace(QStringLiteral("\\"), QStringLiteral("\\\\"));
+        body_content.replace(QStringLiteral("`"), QStringLiteral("\\`"));
+
+        auto js = QStringLiteral(
+                      "var scrollY = window.scrollY;"
+                      "document.body.innerHTML = `%1`;"
+                      "window.scrollTo(0, scrollY);")
+                      .arg(body_content);
+
+        preview_->page()->runJavaScript(js);
     }
 };
 
