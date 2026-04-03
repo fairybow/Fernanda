db6b743ac33b5e05351e3f2a46eb3a45e5d81a43
jackson <63016612+fairybow@users.noreply.github.com>
2026-04-03 16:46:12 -0400

    Update CopyAndFlattenPartialRepo.bat

--- Fernanda/scripts/CopyAndFlattenPartialRepo.bat
+++ Fernanda/scripts/CopyAndFlattenPartialRepo.bat
@@ -4,7 +4,8 @@ setlocal
 set SRC_DIR=%~dp0..\src
 set COCO_INCLUDE_DIR=%~dp0..\submodules\Coco\Coco\include\Coco
 set COCO_SRC_DIR=%~dp0..\submodules\Coco\Coco\src
-set FOUNTAIN_H_SRC_DIR=%~dp0..\submodules\Fountain.h\include
+set FOUNTAIN_H_INCLUDE_DIR=%~dp0..\submodules\fountain.h\fountain.h\include
+set FOUNTAIN_H_SRC_DIR=%~dp0..\submodules\fountain.h\fountain.h\src
 set DOCS_DIR=%~dp0..\docs
 set PROJ_DIR=%~dp0..
 set ROOT_DIR=%~dp0..\..
@@ -18,6 +19,10 @@ for /r "%COCO_INCLUDE_DIR%" %%f in (*) do (
 )
 
 for /r "%COCO_SRC_DIR%" %%f in (*) do (
+    copy "%%f" "%OUT_DIR%\" > nul
+)
+
+for /r "%FOUNTAIN_H_INCLUDE_DIR%" %%f in (*) do (
     copy "%%f" "%OUT_DIR%\" > nul
 )
 
